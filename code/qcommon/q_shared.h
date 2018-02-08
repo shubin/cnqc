14f5b4e06d4fe512ec7618b446254d39a8eac621
myT <mighty.cow@mailinator.com>
2018-02-08 06:43:00 +0100

    bumped the version number up

--- code/qcommon/q_shared.h
+++ code/qcommon/q_shared.h
@@ -26,7 +26,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 // q_shared.h -- included first by ALL program modules.
 // A user mod should never modify this file
 
-#define Q3_VERSION				"CNQ3 1.49"
+#define Q3_VERSION				"CNQ3 1.50"
 #define CLIENT_WINDOW_TITLE		"CNQ3"
 #define CONSOLE_WINDOW_TITLE	"CNQ3 Console"
 
