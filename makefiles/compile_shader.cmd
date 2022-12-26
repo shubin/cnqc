eb5cb65e3bd0f591eefbda273b5ff58316e77492
myT <>
2022-12-26 22:04:52 +0100

    added VC++ 2022 project files

--- makefiles/compile_shader.cmd
+++ makefiles/compile_shader.cmd
@@ -1,4 +1,5 @@
 @echo off
 set fxc="%FXCPATH%"
-if "%FXCPATH%"=="" set fxc="%DXSDK_DIR%Utilities\\bin\\x86\\fxc.exe"
+if "%FXCPATH%"=="" if not "%DXSDK_DIR%"=="" set fxc="%DXSDK_DIR%Utilities\\bin\\x86\\fxc.exe"
+if "%FXCPATH%"=="" if     "%DXSDK_DIR%"=="" set fxc="C:\\Program Files (x86)\\Windows Kits\\10\\bin\\10.0.20348.0\\x64\\fxc.exe"
 %fxc% %*
