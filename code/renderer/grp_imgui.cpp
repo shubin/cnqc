c28485116033f1fe6110958bdf9ea34372ff2586
myT <>
2023-01-08 03:54:47 +0100

    font atlas created

--- code/renderer/grp_imgui.cpp
+++ code/renderer/grp_imgui.cpp
@@ -103,163 +103,33 @@ HRootSignature rootSignature;
 HPipeline pipeline;
 
 
-static void ImGui_ImplDX12_CreateFontsTexture()
+
+static void CreateFontAtlas()
 {
-#if 0
-	// Build texture atlas
 	ImGuiIO& io = ImGui::GetIO();
-	ImGui_ImplDX12_Data* bd = ImGui_ImplDX12_GetBackendData();
 	unsigned char* pixels;
 	int width, height;
 	io.Fonts->GetTexDataAsRGBA32(&pixels, &width, &height);
 
-	// Upload texture to graphics system
-	{
-		D3D12_HEAP_PROPERTIES props;
-		memset(&props, 0, sizeof(D3D12_HEAP_PROPERTIES));
-		props.Type = D3D12_HEAP_TYPE_DEFAULT;
-		props.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
-		props.MemoryPoolPreference = D3D12_MEMORY_POOL_UNKNOWN;
+	TextureDesc desc = { 0 };
+	desc.name = "Dear ImGUI font atlas";
+	desc.initialState = ResourceState::PixelShaderAccessBit;
+	desc.allowedState = ResourceState::PixelShaderAccessBit;
+	desc.width = width;
+	desc.height = height;
+	desc.mipCount = 1;
+	desc.sampleCount = 1;
+	desc.format = TextureFormat::RGBA32_UNorm;
+	desc.committedResource = true;
+	HTexture fontAtlas = CreateTexture(desc);
 
-		D3D12_RESOURCE_DESC desc;
-		ZeroMemory(&desc, sizeof(desc));
-		desc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
-		desc.Alignment = 0;
-		desc.Width = width;
-		desc.Height = height;
-		desc.DepthOrArraySize = 1;
-		desc.MipLevels = 1;
-		desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-		desc.SampleDesc.Count = 1;
-		desc.SampleDesc.Quality = 0;
-		desc.Layout = D3D12_TEXTURE_LAYOUT_UNKNOWN;
-		desc.Flags = D3D12_RESOURCE_FLAG_NONE;
+	TextureUpload upload = { 0 };
+	upload.width = width;
+	upload.height = height;
+	upload.data = pixels;
+	UploadTextureMip0(fontAtlas, upload);
 
-		ID3D12Resource* pTexture = nullptr;
-		bd->pd3dDevice->CreateCommittedResource(&props, D3D12_HEAP_FLAG_NONE, &desc,
-			D3D12_RESOURCE_STATE_COPY_DEST, nullptr, IID_PPV_ARGS(&pTexture));
-
-		UINT uploadPitch = (width * 4 + D3D12_TEXTURE_DATA_PITCH_ALIGNMENT - 1u) & ~(D3D12_TEXTURE_DATA_PITCH_ALIGNMENT - 1u);
-		UINT uploadSize = height * uploadPitch;
-		desc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
-		desc.Alignment = 0;
-		desc.Width = uploadSize;
-		desc.Height = 1;
-		desc.DepthOrArraySize = 1;
-		desc.MipLevels = 1;
-		desc.Format = DXGI_FORMAT_UNKNOWN;
-		desc.SampleDesc.Count = 1;
-		desc.SampleDesc.Quality = 0;
-		desc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
-		desc.Flags = D3D12_RESOURCE_FLAG_NONE;
-
-		props.Type = D3D12_HEAP_TYPE_UPLOAD;
-		props.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
-		props.MemoryPoolPreference = D3D12_MEMORY_POOL_UNKNOWN;
-
-		ID3D12Resource* uploadBuffer = nullptr;
-		HRESULT hr = bd->pd3dDevice->CreateCommittedResource(&props, D3D12_HEAP_FLAG_NONE, &desc,
-			D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(&uploadBuffer));
-		IM_ASSERT(SUCCEEDED(hr));
-
-		void* mapped = nullptr;
-		D3D12_RANGE range = { 0, uploadSize };
-		hr = uploadBuffer->Map(0, &range, &mapped);
-		IM_ASSERT(SUCCEEDED(hr));
-		for(int y = 0; y < height; y++)
-			memcpy((void*)((uintptr_t)mapped + y * uploadPitch), pixels + y * width * 4, width * 4);
-		uploadBuffer->Unmap(0, &range);
-
-		D3D12_TEXTURE_COPY_LOCATION srcLocation = {};
-		srcLocation.pResource = uploadBuffer;
-		srcLocation.Type = D3D12_TEXTURE_COPY_TYPE_PLACED_FOOTPRINT;
-		srcLocation.PlacedFootprint.Footprint.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-		srcLocation.PlacedFootprint.Footprint.Width = width;
-		srcLocation.PlacedFootprint.Footprint.Height = height;
-		srcLocation.PlacedFootprint.Footprint.Depth = 1;
-		srcLocation.PlacedFootprint.Footprint.RowPitch = uploadPitch;
-
-		D3D12_TEXTURE_COPY_LOCATION dstLocation = {};
-		dstLocation.pResource = pTexture;
-		dstLocation.Type = D3D12_TEXTURE_COPY_TYPE_SUBRESOURCE_INDEX;
-		dstLocation.SubresourceIndex = 0;
-
-		D3D12_RESOURCE_BARRIER barrier = {};
-		barrier.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
-		barrier.Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE;
-		barrier.Transition.pResource = pTexture;
-		barrier.Transition.Subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES;
-		barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_COPY_DEST;
-		barrier.Transition.StateAfter = D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE;
-
-		ID3D12Fence* fence = nullptr;
-		hr = bd->pd3dDevice->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence));
-		IM_ASSERT(SUCCEEDED(hr));
-
-		HANDLE event = CreateEvent(0, 0, 0, 0);
-		IM_ASSERT(event != nullptr);
-
-		D3D12_COMMAND_QUEUE_DESC queueDesc = {};
-		queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
-		queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
-		queueDesc.NodeMask = 1;
-
-		ID3D12CommandQueue* cmdQueue = nullptr;
-		hr = bd->pd3dDevice->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&cmdQueue));
-		IM_ASSERT(SUCCEEDED(hr));
-
-		ID3D12CommandAllocator* cmdAlloc = nullptr;
-		hr = bd->pd3dDevice->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&cmdAlloc));
-		IM_ASSERT(SUCCEEDED(hr));
-
-		ID3D12GraphicsCommandList* cmdList = nullptr;
-		hr = bd->pd3dDevice->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, cmdAlloc, nullptr, IID_PPV_ARGS(&cmdList));
-		IM_ASSERT(SUCCEEDED(hr));
-
-		cmdList->CopyTextureRegion(&dstLocation, 0, 0, 0, &srcLocation, nullptr);
-		cmdList->ResourceBarrier(1, &barrier);
-
-		hr = cmdList->Close();
-		IM_ASSERT(SUCCEEDED(hr));
-
-		cmdQueue->ExecuteCommandLists(1, (ID3D12CommandList* const*)&cmdList);
-		hr = cmdQueue->Signal(fence, 1);
-		IM_ASSERT(SUCCEEDED(hr));
-
-		fence->SetEventOnCompletion(1, event);
-		WaitForSingleObject(event, INFINITE);
-
-		cmdList->Release();
-		cmdAlloc->Release();
-		cmdQueue->Release();
-		CloseHandle(event);
-		fence->Release();
-		uploadBuffer->Release();
-
-		// Create texture view
-		D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc;
-		ZeroMemory(&srvDesc, sizeof(srvDesc));
-		srvDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-		srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
-		srvDesc.Texture2D.MipLevels = desc.MipLevels;
-		srvDesc.Texture2D.MostDetailedMip = 0;
-		srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
-		bd->pd3dDevice->CreateShaderResourceView(pTexture, &srvDesc, bd->hFontSrvCpuDescHandle);
-		SafeRelease(bd->pFontTextureResource);
-		bd->pFontTextureResource = pTexture;
-	}
-
-	// Store our identifier
-	// READ THIS IF THE STATIC_ASSERT() TRIGGERS:
-	// - Important: to compile on 32-bit systems, this backend requires code to be compiled with '#define ImTextureID ImU64'.
-	// - This is because we need ImTextureID to carry a 64-bit value and by default ImTextureID is defined as void*.
-	// [Solution 1] IDE/msbuild: in "Properties/C++/Preprocessor Definitions" add 'ImTextureID=ImU64' (this is what we do in the 'example_win32_direct12/example_win32_direct12.vcxproj' project file)
-	// [Solution 2] IDE/msbuild: in "Properties/C++/Preprocessor Definitions" add 'IMGUI_USER_CONFIG="my_imgui_config.h"' and inside 'my_imgui_config.h' add '#define ImTextureID ImU64' and as many other options as you like.
-	// [Solution 3] IDE/msbuild: edit imconfig.h and add '#define ImTextureID ImU64' (prefer solution 2 to create your own config file!)
-	// [Solution 4] command-line: add '/D ImTextureID=ImU64' to your cl.exe command-line (this is what we do in the example_win32_direct12/build_win32.bat file)
-	static_assert(sizeof(ImTextureID) >= sizeof(bd->hFontSrvGpuDescHandle.ptr), "Can't pack descriptor handle into TexID, 32-bit not supported yet.");
-	io.Fonts->SetTexID((ImTextureID)bd->hFontSrvGpuDescHandle.ptr);
-#endif
+	io.Fonts->SetTexID((ImTextureID)fontAtlas.v);
 }
 
 
@@ -339,7 +209,7 @@ void imgui_t::Init()
 		pipeline = CreateGraphicsPipeline(desc);
 	}
 
-	ImGui_ImplDX12_CreateFontsTexture();
+	CreateFontAtlas();
 }
 
 void imgui_t::Draw()
