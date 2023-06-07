511a0ba80d7a32eeaeb0db319c30789ec3fe7ef7
serge_shubin <serge@generalarcade.com>
2023-06-07 16:05:28 +0700

    Increased max number of weapons

--- code/qcommon/q_shared.h
+++ code/qcommon/q_shared.h
@@ -853,7 +853,11 @@ typedef struct {
 #define	MAX_STATS				16
 #define	MAX_PERSISTANT			16
 #define	MAX_POWERUPS			16
+#if defined( QC )
+#define MAX_WEAPONS				24
+#else // QC
 #define	MAX_WEAPONS				16
+#endif // QC
 
 #define	MAX_PS_EVENTS			2
 
